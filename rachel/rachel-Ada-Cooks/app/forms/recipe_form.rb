9679016fecd9cfe89c3c5d269dec7c10e889b292
Rachel <rmoshier@gmail.com>
2014-11-07 16:33:18 -0800

    recipe_form update

--- rachel/rachel-Ada-Cooks/app/forms/recipe_form.rb
+++ rachel/rachel-Ada-Cooks/app/forms/recipe_form.rb
@@ -13,7 +13,22 @@ class RecipeForm
     # create recipe ingredients for each id in ingredients key
     @recipe = Recipe.create(
     name:        @attributes[:name],
-    description: @attributes[:description]
+    description: @attributes[:description],
+    procedure:   @attributes[:procedure]
+    )
+
+    @attributes[:ingredients].each do |id|
+      RecipeIngredient.create(ingredient_id: id, recipe_id: @recipe.id)
+    end
+    @recipe.valid?
+  end
+
+  def destroy(recipe_id)
+    @recipe = Recipe.find(recipe_id)
+    @recipe.update(
+    name:        @attributes[:name],
+    description: @attributes[:description],
+    procedure:   @attributes[:procedure]
     )
 
     @attributes[:ingredients].each do |id|
